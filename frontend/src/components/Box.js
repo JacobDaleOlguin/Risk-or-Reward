a895f9dc9380087632c623878fc03907180aaca8
JacobDaleOlguin <JacobDaleOlguin@gmail.com>
2024-04-19 20:38:58 -0400

    additioanl fixes to production v1

--- frontend/src/components/Box.js
+++ frontend/src/components/Box.js
@@ -1,5 +1,4 @@
-// Assuming this component is now Box.js
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 import { useFrame, useThree } from '@react-three/fiber';
 import { useTexture } from '@react-three/drei';
 
@@ -7,32 +6,32 @@ function Box(props) {
   const mesh = useRef();
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const { size, viewport } = useThree();
-  const texture = useTexture('/Rimage.webp'); // Adjust the path based on your project structure
+  const texture = useTexture('/Rimage.webp');
 
-  // Function to update mouse position
-  const updateMousePosition = (x, y) => {
+  const updateMousePosition = useCallback((x, y) => {
     setMousePosition({
       x: (x / size.width) * 2 - 1,
       y: -(y / size.height) * 2 + 1
     });
-  };
+  }, [size.width, size.height]);
 
   useEffect(() => {
     const handleMouseMove = (event) => {
       updateMousePosition(event.clientX, event.clientY);
     };
-  
+
     window.addEventListener('mousemove', handleMouseMove);
-  
+
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [size.width, size.height, updateMousePosition]); // Now updateMousePosition is a dependency
+  }, [updateMousePosition]); // Dependency on the memoized function
 
   useFrame(() => {
-    // Use mouse position to modify the mesh rotation
-    mesh.current.rotation.x = mousePosition.y * viewport.height * 0.1;
-    mesh.current.rotation.y = mousePosition.x * viewport.width * 0.2;
+    if (mesh.current) {
+      mesh.current.rotation.x = mousePosition.y * viewport.height * 0.1;
+      mesh.current.rotation.y = mousePosition.x * viewport.width * 0.2;
+    }
   });
 
   return (
